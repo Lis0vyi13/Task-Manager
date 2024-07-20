3be7e30ab5029b7b9b857f74620dfa5e2e5e992f
Olexandr Lisovyi <microfox141120@outlook.com>
2024-07-20 16:00:10 +0300

    Updates

--- assets/useSearch-B7-LRE_H.js
+++
@@ -1,1 +0,0 @@
-import{u as r}from"./index-DmyOLR6c.js";const o=()=>r(e=>e.search.query);export{o as u};
---
+++ assets/useSearch-DcyhTdjs.js
@@ -0,0 +1,1 @@
+import{u as r}from"./index-BYnzJnnp.js";const o=()=>r(e=>e.search.query);export{o as u};
